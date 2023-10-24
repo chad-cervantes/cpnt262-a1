187b8f3bc02964a7da74aec74d1edda6bd9754ce
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-24 16:36:19 -0600

    added the query selectors as const for primary nav toggle

--- client.js
+++ client.js
@@ -1,1 +1,6 @@
+//DOM elements for primary nav toggle
+const menuIcon = document.querySelector("menu-icon");
+const gridContainer = document.querySelector("#grid-container");
+const siteHeader = document.querySelector("#site-header");
+const mobileMediaQuery = window.watchMedia("(max-width:550)");
 
