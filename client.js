8f38df32cf73e5bbafa763a193593ef36045e347
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-24 16:40:18 -0600

    added funtion of primary nav toggle

--- client.js
+++ client.js
@@ -4,3 +4,10 @@ const gridContainer = document.querySelector("#grid-container");
 const siteHeader = document.querySelector("#site-header");
 const mobileMediaQuery = window.watchMedia("(max-width:550)");
 
+//Functions
+
+function applyToggleTransition(inputQuery) {
+  if (inputQuery.matches) {
+    gridContainer.classList.remove("menu-row");
+  }
+}
