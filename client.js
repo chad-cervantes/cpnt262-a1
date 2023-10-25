1e7ba99b596fa5542ab3364568d3d07d6d8e89d5
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-25 11:18:08 -0600

    added objects to the bunka, gyuto, and santoku knives

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 //DOM elements for primary nav toggle
-const menuIcon = document.querySelector("menu-icon");
+const menuIcon = document.querySelector("#menu-icon-container");
 const gridContainer = document.querySelector("#grid-container");
 const siteHeader = document.querySelector("#site-header");
 const mobileMediaQuery = window.watchMedia("(max-width:550)");
@@ -27,24 +27,32 @@ document.addEventListener("click", (e) => {
   }
 });
 
+
 //Render a card/image gallery
 
-//Make arrays for image/card gallery
-const knifeTitleArray  = ["Bunka", "Gyuto", "Santoku", "Petty", "Usaba", "Nakiri", "Deba", "Yanagiba", "Garasuki", "Sujihiki", "Kiritsuke", "Burja"] 
+//Make objects for image/card gallery
+const bunkaKnife  = 
+{name: "Bunka", price: "$100", description: "Like a Santoku knife, but a little longer"}
 
-const knifeDescriptionArray = ["Like a Santoku knife, but a little longer", "Main chef's kitchen knife", "Knife for vegetables, meats, & fish", "Paring knife", "Like a nakiri knife, but smaller", "Thin rectangular vegetable knife", "Heavy knife for bigger tasks", "Cuts thin slices of fish & large cuts of meat", "Designed to slice boneless meat", "Designed to slice fish, vegetables, & fruit", "Prosciutto knife"]
+const gyutoKnife = {name: "Gyuto", price: "$100", description: "Main chef's kitchen knife"}
 
-//Make querySelectors
-let cards = document.querySelector('.card-container');
+const santokuKnife = {name: "Santoku", price: "$100", description: "Knife for vegetables, meats, & fish"}
+
+// ["Bunka", "Gyuto", "Santoku", "Petty", "Usaba", "Nakiri", "Deba", "Yanagiba", "Garasuki", "Sujihiki", "Kiritsuke", "Burja"] 
+
+// const knifeDescriptionArray = ["Like a Santoku knife, but a little longer", "Main chef's kitchen knife", "Knife for vegetables, meats, & fish", "Paring knife", "Like a nakiri knife, but smaller", "Thin rectangular vegetable knife", "Heavy knife for bigger tasks", "Cuts thin slices of fish & large cuts of meat", "Designed to slice boneless meat", "Designed to slice fish, vegetables, & fruit", "Prosciutto knife"]
+
+//Make function & querySelectors
+// let cards = document.querySelector('.card-container');
  
-for (let i = 0; i < knifeTitleArray.length; i++) {
+// for (let i = 0; i < knifeTitleArray.length; i++) {
     
-  let fetch = fetch(document.querySelector('.card-container').innerHTML);
-    cards.innerHTML = `<div class="cards">
-    <div class="card-content">
-      <h2>${knifeTitleArray[i]}</h2>
-      <p>${knifeDescriptionArray[i]}</p>
-      <a class="cta" href="#"><u>Check out brands & prices</u></a>
-    </div>
-  </div>` + fetch; 
-}
+//   let fetch = fetch(document.querySelector('.card-container').innerHTML);
+//     cards.innerHTML = `<div class="cards">
+//     <div class="card-content">
+//       <h2>${knifeTitleArray[i]}</h2>
+//       <p>${knifeDescriptionArray[i]}</p>
+//       <a class="cta" href="#"><u>Check out brands & prices</u></a>
+//     </div>
+//   </div>` + fetch; 
+// }
