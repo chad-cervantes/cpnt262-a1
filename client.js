67a7e18677cf95c4691d1adb4088ba4471927524
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-25 20:42:50 -0600

    made new js elements for the day/night toggle section

--- client.js
+++ client.js
@@ -90,6 +90,35 @@ const knifeCards = knives.map((knife)) => {
 //render the knives
 document.querySelector("#knife-list").innerHTML = knifeCards.map((knife) => knife).join("");
 
+//NIGHT TOGGLE
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme');
+
+//if-statement(s)
+
+if(currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if(currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+//function(s)
+function switchTheme(e) {
+  if(e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
 
 // ["Bunka", "Gyuto", "Santoku", "Petty", "Usaba", "Nakiri", "Deba", "Yanagiba", "Garasuki", "Sujihiki", "Kiritsuke", "Burja"] 
 
